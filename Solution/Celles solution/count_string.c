82f64c327edc6807f823a785cefa2ac53db78b5e
SirriCelles <sirricelles@gmail.com>
2018-08-07 15:04:04 +0100

    first string program in c

---
+++ Solution/Celles solution/count_string.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	char sentence[100];
+	printf("Enter the sentence: \n");
+	scanf("%s",sentence);
+	printf("You entered:  %s",sentence);
+	
+
+	return 0;
+}
